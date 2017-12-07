4d5b1f2e70e2259fb3ac7fd69f9f759209df736b
Feroz Ahmmed <froghramar@gmail.com>
2017-12-07 09:48:20 +0600

    add menubar

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+import { BrowserRouter as Router } from 'react-router-dom';
+import { App } from './app/App';
+
 ReactDOM.render(
-    <App />,
+    (<Router>
+        <App />
+    </Router>),
     document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
